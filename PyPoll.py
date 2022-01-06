215c8fed9b522c9711802aa910a4fe4b46531de4
Alexandra Galikeeva <alexandragalikeeva@Alexandras-MacBook-Pro.local>
2022-01-05 20:17:51 -0700

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -22,11 +22,67 @@ with open(file_to_save, "w") as txt_file:
     # Write three counties to the file.
     txt_file.write("Counties in the Election\n----------------------\nArapahoe\nDenver\nJefferson\n")
 
+# Assign variable for total votes
+total_votes=0
+
+# Declare candidate list
+candidate_options=[]
+
+# Declare candidate votes dictionary
+candidate_votes={}
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
     # Read and print the header row in the CSV file.
     headers = next(file_reader)
-    print(headers)
-    
+
+    #Print each row
+    for row in file_reader:
+        # Add to total vote count
+        total_votes+=1
+        
+        # Print candidate names from each row
+        candidate_name=row[2]
+        # If the candidate does not match any existing candidate
+        if candidate_name not in candidate_options:
+            # Add to list of candidates 
+            candidate_options.append(candidate_name)
+            # Create each candidate as key and track each candidate's vote
+            candidate_votes[candidate_name] = 0
+        # Add a count to candidate each time appears in row
+        candidate_votes[candidate_name] += 1
+
+# Determine percentage of votes for each candidate
+# Iterate through candidate list
+for candidate_name in candidate_votes:
+    # Retrieve votes of candidate
+    votes=candidate_votes[candidate_name]
+    # Calculate percentage
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # Print out each candidate's name, vote count, and percentage of votes to the terminal.
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    # Determine the winning candidate by the number and percentage of votes.   
+    # Determine if the vote count that was calculated is greater than the winning_count.
+    if (votes > winning_count) and (vote_percentage> winning_percentage):
+        # 2. If true then set winning_count = votes and winning_percent = # vote_percentage.
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # 3. Set the winning_candidate equal to the candidate's name.
+        winning_candidate = candidate_name
+
+# Print winning candidate, vote count and percentage
+winning_candidate_summary = (
+    f"--------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}\n"
+    f"--------------------\n")
+print(winning_candidate_summary)
--- Python_practice.py
+++ Python_practice.py
@@ -13,3 +13,39 @@ else:
 
 for i in range(len(counties)):
     print(counties[i])
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},{"county":"Denver", "registered_voters": 463353},{"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    print(county_dict)
+
+for i in range(len(voting_data)):
+    print(voting_data[i]['county'])
+
+for county_dict in voting_data:
+    for value in county_dict.values():
+        print(value)
+
+for county_dict in voting_data:  
+     print(county_dict.values())
+
+for county_dict in voting_data:    
+   for value in county_dict:      
+       print(value)
+
+for county_dict in voting_data:
+     print(county_dict['registered_voters'])
+
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes} number of votes. "
+    f"The total number of votes in the election was {total_votes}. "
+    f"You received {candidate_votes / total_votes * 100}% of the total votes.")
+
+print(message_to_candidate)
+
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters:,} registered voters")
+
+voting_data = [``{"county":"Arapahoe", "registered_voters": 422829}, {"county":"Denver", "registered_voters": 463353}, {"county":"Jefferson", "registered_voters": 432438}]
